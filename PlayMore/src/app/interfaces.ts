4d548949641b7934674fae41d1a4934abb98bec8
r-chambers <chambers.r@icloud.com>
2023-09-23 13:35:34 -0400

    Actually added interfaces

---
+++ PlayMore/src/app/interfaces.ts
@@ -0,0 +1,42 @@
+export interface Game {
+    id : string;
+    name : string;
+    cover_url : string;
+    date_released : string;
+    idgb_rating : number;
+    pm_rating : number;
+}
+
+export interface Review {
+    id : string;
+    game_id : string;
+    title : string;
+    text : string;
+    date_created : string;
+    feature_ratings : Feature_Rating[];
+}
+
+export interface Feature_Rating {
+    id : string;
+    dis_id : string;
+    rating : number;
+}
+
+export interface Disability {
+    id : string;
+    name : string;
+    features : Feature[];
+}
+
+export interface Feature {
+    id : string;
+    name : string;
+}
+
+export interface User {
+    id : string;
+    username : string;
+    date_joined : string;
+    avatar_url : string;
+    dis : Disability[];
+}
